d50b5fbce12341041ae0eba93b0c3900fbd159fb
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2022-11-13 16:52:41 -0500

    Event Ding React Basic Architecture Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LazyLoader from './components/LazyLoader/LazyLoader';
 
 function App() {
+
+  const Home = lazy(() => import('./components/Home/Home'));
+  const NotFound = lazy(() => import('./components/NotFound/NotFound'));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={LazyLoader()}>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/home" element={<Home />} />
+      <Route element={<NotFound />} />
+    </Routes>
+  </Suspense>
   );
 }
 
