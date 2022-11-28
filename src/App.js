502855ae5da986050532a40d6e2ec675c3137f84
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2022-11-28 18:23:37 -0500

    Signup route

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ function App() {
 
   const Home = lazy(() => import('./components/Home/Home'));
   const Login = lazy(() => import('./components/Login/Login'));
+  const Signup = lazy(() => import('./components/Signup/Signup'));
   const NotFound = lazy(() => import('./components/NotFound/NotFound'));
 
   return (
     <Suspense fallback={LazyLoader()}>
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/login" element={<Login />} />
-      <Route element={<NotFound />} />
-    </Routes>
-  </Suspense>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 }
 
