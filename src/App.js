6154406532fe12668a015dc4016cb9ca6e3f6e18
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2022-12-05 16:03:24 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -8,14 +8,20 @@ function App() {
   const Login = lazy(() => import("./components/Login/Login"));
   const Signup = lazy(() => import("./components/Signup/Signup"));
   const NotFound = lazy(() => import("./components/NotFound/NotFound"));
+  const Dashboard = lazy(() => import("./components/Dashboard/Dashboard"));
 
   return (
     <Suspense fallback={LazyLoader()}>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
-        <Route exact path="/redirect"  element={ <Navigate replace to="/signup" /> } />
+        <Route exact path="/redirect" element={<Navigate replace to="/signup" />} />
         <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/dashboard">
+          <Dashboard>
+            <NotFound />
+          </Dashboard>
+        </Route>
         <Route element={<NotFound />} />
       </Routes>
     </Suspense>
---
+++ src/configs/axiosConfig.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import localStorageService from './LocalStorageService';
+
+const instance = axios.create({
+    // baseURL: 'http://95.216.229.35:8228/institute'
+    baseURL: 'http://localhost:8000/events'
+    // baseURL: 'https://web.classbridgeapp.com/institute'
+});
+
+instance.interceptors.request.use(
+    config => {
+        config.headers['Authorization'] = localStorageService.getToken();
+        config.headers['Auth-type'] = 'token';
+
+        return config;
+    },
+    error => {
+        return error;
+    });
+
+export default instance;
