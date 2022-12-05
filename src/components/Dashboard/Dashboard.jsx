92d55f009d5cc5fdf0d7b2e68855c76cae3ba624
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2022-12-05 16:16:46 -0500

    Changes

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import LazyLoader from '../LazyLoader/LazyLoader';
 
-function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+function Dashboard(props) {
+    return (
+        <div>
+            {props.children}
+        </div>
+    )
 }
 
 export default Dashboard
