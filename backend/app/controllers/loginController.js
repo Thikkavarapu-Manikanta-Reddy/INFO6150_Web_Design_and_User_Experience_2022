64cff0516c1ed505a3d3469d89928d82adc44f2d
padmaanaokar <anaokar.p@northeastern.edu>
2022-12-05 23:50:01 -0500

    Changes added

--- backend/app/controllers/loginController.js
+++ backend/app/controllers/loginController.js
@@ -1,11 +1,29 @@
 const bcrypt = require('bcrypt');
 const loginServices = require('../services/loginServices');
-const loginUserModel = require('../model/loginUserModel');
-const validator = require('../utilities/Validator');
+const loginUser = require('../model/loginUser');
+//const User = require('../model/User')
+const validator = require('../utilities/Validators');
 
-const login = (req, res) => {
+const login = async(req, res) => {
     try {
+        validator.validateEmail(req.body.email);
+        validator.validatePassword(req.body.password);
 
+        let salt = await bcrypt.genSalt(15);
+        let hash = await bcrypt.hash(req.body.password, salt);
+
+        req.body.password = hash;
+
+        const user = new loginUser(req.body);
+
+        loginServices.loginUser(user).then(result => {
+                
+            if (result != null)
+                res.json("User created Successfully");
+        }).catch(err => {
+            res.status(400);
+            res.json({ "message": err.message });
+        });
     }
     catch (err) {
         res.status(400);
