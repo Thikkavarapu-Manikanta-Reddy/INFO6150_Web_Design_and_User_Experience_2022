9b1a51da553dc9eaa6fe69d9211e78c963782d38
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2022-12-07 19:58:02 -0500

    Changes

--- src/components/ChildRoutes/Dashboard/Dashboard.jsx
+++ src/components/ChildRoutes/Dashboard/Dashboard.jsx
@@ -7,22 +7,18 @@ import EditIcon from '@mui/icons-material/Edit';
 function Dashboard({ user }) {
 
     const addEvent = () => {
-        
+
     }
 
     return (
         <div>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="mobileCardCenterView col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                    <div style={{marginTop: "20px"}} className="mobileCardCenterView col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
                         <h5 className="secondaryColor fontBoldSmall"><i className="fas fa-user-graduate"></i>&nbsp;&nbsp;Event hosting</h5>
-                    </div>
-                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center">
-                    </div>
-                    <div className="mobileCardCenterView col-xs-12 col-sm-12 col-md-3 col-lg-3">
                         {
                             user.role === 'Admin' ?
-                                <div className="floatRight">
+                                <div>
                                     <Fab style={{ backgroundColor: "#0e76a8" }} onClick={addEvent} size="small" color="primary" aria-label="add">
                                         <AddIcon />
                                     </Fab>&nbsp;&nbsp;
