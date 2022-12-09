ece7919d690c73fb78fc8b16c8b4f840006022af
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2022-12-08 23:27:03 -0500

    Cahnges

--- src/components/ChildRoutes/EventHost/EventHost.jsx
+++ src/components/ChildRoutes/EventHost/EventHost.jsx
@@ -16,14 +16,35 @@ import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import CancelIcon from '@mui/icons-material/Cancel';
+import Button from '@mui/material/Button';
+import eventCreation from "../../../assets/event_creation.svg";
+import Divider from '@mui/material/Divider';
 
 function EventHost(props) {
 
     const [dateAndTimeValue, setDateAndTimeValue] = React.useState(dayjs(new Date()));
 
+    const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
     const handleDateAndTimeChange = (newValue) => {
         setDateAndTimeValue(newValue);
+        let dateAndTime = constructDateAndTimeString(newValue);
+        console.log(dateAndTime);
+        setEventData({ ...eventData, dateAndTime });
     };
+
+    const constructDateAndTimeString = (dateObj) => {
+        let hour, amOrPm;
+        if (dateObj.$H > 12) {
+            hour = dateObj.$H - 12;
+            amOrPm = "pm";
+        }
+        else {
+            hour = dateObj.$H;
+            amOrPm = "am";
+        }
+        return `${dateObj.$D}` + " " + `${month[dateObj.$M]}` + " " + `${dateObj.$y}` + " - " + `${hour}` + `${dateObj.$m}` + " " + `${amOrPm}`;
+    }
 
     const [eventData, setEventData] = useState({
         title: '',
@@ -69,6 +90,10 @@ function EventHost(props) {
         }
     }
 
+    const saveEvent = () => {
+
+    }
+
     const getEventData = (e) => {
         setEventData({ ...eventData, [e.target.name]: e.target.value });
         console.log(eventData);
@@ -93,81 +118,117 @@ function EventHost(props) {
                     <div className="row">
                         <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
                             <p className="secondary-color fontBoldSmall">Event Creation</p>
-                            <hr style={{ width: "20%", margin: "0" }} />
+                            <hr style={{ width: "30%", margin: "0" }} />
                         </div>
                         <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
                             <CancelIcon onClick={props.toggleEventModal} className="secondary-color" style={{ float: "right", cursor: "pointer" }} fontSize="large" />
                         </div>
                     </div>
+
                     <div className="row">
-                        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4"><br />
-                            <TextField
-                                autoComplete="event-title"
-                                name="title"
-                                required
-                                fullWidth
-                                id="eventTitle"
-                                label="Event Title"
-                                autoFocus
-                                placeholder="Event Title" type="text" value={eventData.title} onChange={getEventData}
-                            />
+                        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <img className="imageSize" src={eventCreation} alt="Not_Found" /><br />
                         </div>
-                        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4"><br />
-                            <FormControl variant="outlined" style={{ width: "100%" }}>
-                                <InputLabel id="demo-simple-select-outlined-label">Event Type</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-outlined-label"
-                                    id="demo-simple-select-outlined"
-                                    label="Event Type"
-                                    value={eventData.type}
-                                    onChange={getEventData}
-                                    inputProps={{
-                                        name: "type"
-                                    }}
-                                    style={{ height: "50px" }}
-                                >
-                                    {eventTypes.map((option, index) => (
-                                        <MenuItem id={index} key={option.id} value={option.name}>
-                                            {option.name}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
+                        <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1 text-center">
+                            <Divider style={{ height: "100%" }} orientation="vertical" flexItem />
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8"><br />
-                            <TextareaAutosize
-                                name="description"
-                                onChange={getEventData}
-                                aria-label="empty textarea"
-                                placeholder="Description"
-                                minRows={5}
-                                style={{ width: "100%" }}
-                            />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4"><br />
-                            <TextField
-                                autoComplete="ticket-count"
-                                name="ticketCount"
-                                required
-                                fullWidth
-                                id="ticketCount"
-                                label="Ticket Count"
-                                placeholder="Ticket Count" type="text" value={eventData.ticketCount} onChange={getEventData}
-                            />
-                        </div>
-                        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4"><br />
-                            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                <DateTimePicker
-                                    label="Date&Time picker"
-                                    value={dateAndTimeValue}
-                                    onChange={handleDateAndTimeChange}
-                                    renderInput={(params) => <TextField {...params} />}
-                                />
-                            </LocalizationProvider>
+                        <div className="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+                            <div className="row">
+                                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6"><br />
+                                    <TextField
+                                        autoComplete="event-title"
+                                        name="title"
+                                        required
+                                        fullWidth
+                                        id="eventTitle"
+                                        label="Event Title"
+                                        autoFocus
+                                        placeholder="Event Title" type="text" value={eventData.title} onChange={getEventData}
+                                    />
+                                </div>
+                                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6"><br />
+                                    <FormControl variant="outlined" style={{ width: "100%" }}>
+                                        <InputLabel id="demo-simple-select-outlined-label">Event Type</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-outlined-label"
+                                            id="demo-simple-select-outlined"
+                                            label="Event Type"
+                                            value={eventData.type}
+                                            onChange={getEventData}
+                                            inputProps={{
+                                                name: "type"
+                                            }}
+                                            style={{ height: "50px" }}
+                                        >
+                                            {eventTypes.map((option, index) => (
+                                                <MenuItem id={index} key={option.id} value={option.name}>
+                                                    {option.name}
+                                                </MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12"><br />
+                                    <TextareaAutosize
+                                        name="description"
+                                        onChange={getEventData}
+                                        aria-label="empty textarea"
+                                        placeholder="Description"
+                                        minRows={5}
+                                        style={{ width: "100%" }}
+                                    />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center"><br />
+                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                        <DateTimePicker
+                                            label="Date&Time picker"
+                                            value={dateAndTimeValue}
+                                            onChange={handleDateAndTimeChange}
+                                            renderInput={(params) => <TextField {...params} />}
+                                        />
+                                    </LocalizationProvider>
+                                </div>
+                                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6"><br />
+                                    <TextField
+                                        autoComplete="ticket-count"
+                                        name="ticketCount"
+                                        required
+                                        fullWidth
+                                        id="ticketCount"
+                                        label="Ticket Count"
+                                        placeholder="Ticket Count" type="text" value={eventData.ticketCount} onChange={getEventData}
+                                    />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12"><br />
+                                    <div style={{ float: "right" }}>
+                                        <Button disabled={
+                                            eventData.title === '' ||
+                                            eventData.type === '' ||
+                                            eventData.description === '' ||
+                                            eventData.dateAndTime === '' ||
+                                            eventData.ticketCount === ''
+                                        }
+                                            className={
+                                                eventData.title === '' ||
+                                                    eventData.type === '' ||
+                                                    eventData.description === '' ||
+                                                    eventData.dateAndTime === '' ||
+                                                    eventData.ticketCount === ''
+                                                    ? null : "enableButtonSecondaryColors"}
+                                            onClick={saveEvent} style={{ fontSize: "100%", borderRadius: "8px" }} variant="contained">Save</Button>&nbsp;&nbsp;&nbsp;
+                                        <Button
+                                            className="enableButtonSecondaryColors"
+                                            onClick={props.toggleEventModal} style={{ fontSize: "100%", borderRadius: "8px" }} variant="contained">Cancel</Button>
+                                    </div>
+                                    <br />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </ModalBody>
